c00ad206a4cfdc6296898854752554437377e788
kimheeseon0526 <khseon0526@nvaer.com>
2025-11-19 18:21:14 +0900

    251119 redux 관련 어쩌고

---
+++ next_ts_employee_redux_restapi/src/components/EmployeeList.tsx
@@ -0,0 +1,35 @@
+import React, {useContext, useEffect} from 'react';
+import InfoTable from "@/components/InfoTable";
+import {buttonBarStyle} from "./Main";
+import {useDispatch, useSelector} from "react-redux";
+import {handleSelectedId} from "@/redux/slice/employeeSlice";
+import {Rootstate, RootDispatch} from "@/redux/store";
+import {fetchGetEmployeeInfos} from "@/redux/api/employeeAPI";
+
+
+const EmployeeList = () => {
+  const {infos} = useSelector((state: Rootstate) => state.emp);
+  const dispatch = useDispatch<RootDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchGetEmployeeInfos())
+  }, [dispatch])
+  return (
+    <>
+      <div style={buttonBarStyle}>
+        {infos?.map(info => (
+            <button key={info.id}
+                    onClick={() => dispatch(handleSelectedId(info.id))}
+            >{info.name}
+            </button>
+          )
+        )}
+      </div>
+      <InfoTable />
+
+    </>
+
+  );
+};
+
+export default EmployeeList;
